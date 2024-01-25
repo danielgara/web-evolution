39e2c80cb19b5f34f12373cc911675f2bbb2bf10
Daniel Correa <yo@danielgara.com>
2024-01-25 11:23:42 -0500

    fix: html

--- 02-ssr/index.php
+++ 02-ssr/index.php
@@ -5,9 +5,9 @@
 <h2>Home Page - Secret Website</h2>
 
 <form method="post" action="welcome.php">
-  Name: <input type="text" name="name"><br /><br />
-  Password: <input type="password" name="password"><br /><br />
-  <input type="submit">
+  Name: <input type="text" name="name" /><br /><br />
+  Password: <input type="password" name="password" /><br /><br />
+  <input type="submit" />
 </form>
 
 </body>
