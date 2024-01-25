f9c0875e005d2c1f91a4d0b7800aa9d2b54bef2c
Daniel Correa <yo@danielgara.com>
2024-01-25 15:05:05 -0500

    imp: 05

---
+++ 05-pwa/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <RouterLink to="/">Home</RouterLink>&nbsp;
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+
+  <RouterView />
+</template>
