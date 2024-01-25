722dea7c3581554054c8661e5720c83f50f1339f
Daniel Correa <yo@danielgara.com>
2024-01-25 11:22:57 -0500

    imp: 02-ssr

---
+++ 02-ssr/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Home Page - Secret Website</h2>
+
+<form method="post" action="welcome.php">
+  Name: <input type="text" name="name"><br /><br />
+  Password: <input type="password" name="password"><br /><br />
+  <input type="submit">
+</form>
+
+</body>
+</html>
---
+++ 02-ssr/welcome.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Welcome Page - Secret Location</h2>
+
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = htmlspecialchars($_POST['name']); 
+  $password = htmlspecialchars($_POST['password']); 
+  if (empty($password) or $password != '123456') {
+    echo "<p>You cannot enter to this site</b>";
+    exit;
+  } else {
+    echo "<p>Hello, $name! You're the best</b>";
+  }
+}
+
+?>
+
+</body>
+</html>
